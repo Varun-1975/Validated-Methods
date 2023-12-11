8867c4fb0c37b0650f143b0a73d064899cb15568
Varun-1975 <153108013+Varun-1975@users.noreply.github.com>
2023-12-11 18:25:23 +0530

    Update performance.js

--- performance.js
+++ performance.js
@@ -16,36 +16,43 @@ function createTable(data) {
     const table = document.createElement('table');
     table.className = 'data-table'; // Add a class for CSS styling
 
-    data.forEach((row, rowIndex) => {
-        const tr = document.createElement('tr'); // Create a new table row
-        let previousCell = null; // To keep track of the last non-empty cell
+    // Create the header row
+    const headerRow = document.createElement('tr');
+    data[0].forEach((header) => {
+        const th = document.createElement('th');
+        th.textContent = header;
+        headerRow.appendChild(th);
+    });
+    table.appendChild(headerRow);
+
+    // Start with the second row, since the first row is the header
+    data.slice(1).forEach((row, rowIndex) => {
+        const tr = document.createElement('tr');
+        let previousCell = null;
 
         row.forEach((cell, cellIndex) => {
-            if (rowIndex === 1 && cellIndex === 0) {
-                // This is the cell with the image link
+            if (rowIndex === 0 && cellIndex === 0) {
+                // Skip the first cell of the second row, since it's an image
                 const td = document.createElement('td');
-                td.colSpan = row.length; // The image cell spans all columns
+                td.colSpan = row.length;
                 const img = document.createElement('img');
-                img.src = cell; // Set the image source to the link
-                img.alt = 'Image'; // Set an alt text for the image
-                img.style.maxWidth = '100%'; // Ensure the image is not larger than the cell
+                img.src = cell;
+                img.alt = 'Image';
+                img.style.maxWidth = '100%';
                 img.style.height = 'auto';
                 td.appendChild(img);
                 tr.appendChild(td);
-                return; // Skip the rest of the cells in this row
             } else if (cell.trim() === '') {
-                // If the cell is empty, increase the colspan of the previous cell
                 if (previousCell) previousCell.colSpan += 1;
             } else {
-                // If the cell is not empty, create a new cell
                 const td = document.createElement('td');
                 td.textContent = cell;
-                tr.appendChild(td); // Append the cell to the row
-                previousCell = td; // Set this cell as the last non-empty cell
+                tr.appendChild(td);
+                previousCell = td;
             }
         });
 
-        table.appendChild(tr); // Append the row to the table
+        table.appendChild(tr);
     });
 
     return table;
@@ -53,14 +60,13 @@ function createTable(data) {
 
 // Function to initialize the data fetching and table creation
 function initializeTable() {
-    // The URL should point to your TSV data
     const tsvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTi_0g4fsZHLownRxEBfnrAGnzIHRLNOkqVN_ibgrUHDxhXD5WdL3LhlHnrEn0PnGivZjIvjQQ2UL7i/pub?gid=0&single=true&output=tsv'; // Replace with the actual URL
 
     fetchTSVData(tsvUrl).then(data => {
         const container = document.getElementById('data-container');
-        container.innerHTML = ''; // Clear any existing content
+        container.innerHTML = '';
         const table = createTable(data);
-        container.appendChild(table); // Append the new table to the container
+        container.appendChild(table);
     });
 }
 
