551257efd8c216c17a99048d24f5f7ed360f6c2d
Varun-1975 <153108013+Varun-1975@users.noreply.github.com>
2023-12-11 18:01:41 +0530

    Update performance.js

--- performance.js
+++ performance.js
@@ -2,87 +2,45 @@
 async function fetchTSVData(url) {
     const response = await fetch(url);
     const text = await response.text();
+    // Split the text into rows, then split each row into columns
     return text.split('\n').map(row => row.split('\t'));
 }
 
 // Function to create a table with the TSV data
 function createTable(data) {
     const table = document.createElement('table');
+    table.className = 'data-table'; // Add a class for CSS styling
 
+    // Iterate over each row of data
     data.forEach((row, rowIndex) => {
-        const tr = document.createElement('tr');
+        const tr = document.createElement('tr'); // Create a new table row
 
+        // Iterate over each cell in the row
         row.forEach((cell, cellIndex) => {
-            // Create the header row with merged cells
-            if (rowIndex === 0 && cellIndex === 0) {
-                const th = document.createElement('th');
-                th.setAttribute('colspan', '3');
-                th.textContent = cell;
-                th.style.textAlign = 'center';
-                th.style.fontWeight = 'bold';
-                tr.appendChild(th);
-                return; // Skip the rest of the cells in the header row
-            }
-
-            // Handle the image row with merged cells
-            if (rowIndex === 1 && cellIndex === 0) {
-                const th = document.createElement('th');
-                th.setAttribute('colspan', '3');
-                const img = document.createElement('img');
-                img.src = parseImageLink(cell);
-                img.style.display = 'block';
-                img.style.marginLeft = 'auto';
-                img.style.marginRight = 'auto';
-                img.style.width = '200px'; // Set the image size
-                th.appendChild(img);
-                tr.appendChild(th);
-                return; // Skip the rest of the cells in the image row
-            }
-
-            // Skip cells that are meant to be merged
-            if (rowIndex > 1 && cellIndex > 0) return;
-
-            // Handle bold cells and merged cells for the rest of the table
-            const td = document.createElement('td');
-            if (cellIndex === 0) {
-                td.style.fontWeight = 'bold'; // Bold for the first column
-            }
-            if (rowIndex > 1 && cellIndex === 1) {
-                td.setAttribute('colspan', '2'); // Merge horizontally for specified cells
-            }
-            td.textContent = cell;
-            tr.appendChild(td);
+            const cellElement = document.createElement(rowIndex === 0 ? 'th' : 'td');
+            // Set the text content of the cell
+            cellElement.textContent = cell;
+            tr.appendChild(cellElement); // Append the cell to the row
         });
 
-        table.appendChild(tr);
+        table.appendChild(tr); // Append the row to the table
     });
 
     return table;
 }
 
-// Helper function to check if a string is an image link
-function isImageLink(str) {
-    return str.includes('cdn.discordapp.com/attachments/');
-}
-
-// Function to parse the provided image link format
-function parseImageLink(link) {
-    const baseUrl = link.split('?')[0];
-    return baseUrl;
-}
-
 // Function to initialize the data fetching and table creation
 function initializeTable() {
-    const container = document.getElementById('data-container');
-    const tsvUrl = container.getAttribute('data-sheet-url');
+    // The URL should point to your TSV data
+    const tsvUrl = 'https://example.com/path/to/your/data.tsv'; // Replace with the actual URL
+
     fetchTSVData(tsvUrl).then(data => {
-        // Clear previous content
-        container.innerHTML = '';
-        // Create and append the new table
+        const container = document.getElementById('data-container');
+        container.innerHTML = ''; // Clear any existing content
         const table = createTable(data);
-        container.appendChild(table);
+        container.appendChild(table); // Append the new table to the container
     });
 }
 
-// Call the initialize function when the DOM is fully loaded
+// Event listener to run the initializeTable function after the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', initializeTable);
