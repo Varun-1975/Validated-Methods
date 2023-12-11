0e393e81cf44cfbdffddaa376ed6881e133e5120
Varun-1975 <153108013+Varun-1975@users.noreply.github.com>
2023-12-11 18:32:11 +0530

    Update performance.js

--- performance.js
+++ performance.js
@@ -7,7 +7,6 @@ async function fetchTSVData(url) {
 
 // Helper function to check if a string is an image link
 function isImageLink(str) {
-    // Here we're checking if the string is a link to an image based on the provided pattern
     return str.startsWith('https://cdn.discordapp.com/attachments/');
 }
 
@@ -18,41 +17,51 @@ function createTable(data) {
 
     // Create the header row
     const headerRow = document.createElement('tr');
-    data[0].forEach((header) => {
-        const th = document.createElement('th');
-        th.textContent = header;
-        headerRow.appendChild(th);
+    let previousHeaderCell = null; // Keep track of the previous non-empty header cell for merging
+    data[0].forEach((header, index) => {
+        if (index > 0 && header.trim() === '' && previousHeaderCell) {
+            // Increase the colspan for merged header cells
+            previousHeaderCell.colSpan += 1;
+        } else {
+            const th = document.createElement('th');
+            th.textContent = header;
+            headerRow.appendChild(th);
+            previousHeaderCell = th; // Update the last non-empty header cell
+        }
     });
     table.appendChild(headerRow);
 
-    // Start with the second row, since the first row is the header
+    // Process the rest of the data starting from the second row
     data.slice(1).forEach((row, rowIndex) => {
         const tr = document.createElement('tr');
-        let previousCell = null;
+        let previousCell = null; // To keep track of the last non-empty cell for merging
 
         row.forEach((cell, cellIndex) => {
-            if (rowIndex === 0 && cellIndex === 0) {
-                // Skip the first cell of the second row, since it's an image
-                const td = document.createElement('td');
-                td.colSpan = row.length;
-                const img = document.createElement('img');
-                img.src = cell;
-                img.alt = 'Image';
-                img.style.maxWidth = '100%';
-                img.style.height = 'auto';
-                td.appendChild(img);
-                tr.appendChild(td);
-            } else if (cell.trim() === '') {
+            if (cell.trim() === '') {
+                // If the cell is empty, increase the colspan of the previous cell
                 if (previousCell) previousCell.colSpan += 1;
             } else {
-                const td = document.createElement('td');
-                td.textContent = cell;
-                tr.appendChild(td);
-                previousCell = td;
+                const cellTagName = rowIndex === 0 && cellIndex === 0 ? 'th' : 'td';
+                const cellElement = document.createElement(cellTagName);
+                
+                // Set the text content or create an image for the cell
+                if (rowIndex === 0 && isImageLink(cell)) {
+                    const img = document.createElement('img');
+                    img.src = cell;
+                    img.alt = 'Image';
+                    img.style.maxWidth = '100%';
+                    img.style.height = 'auto';
+                    cellElement.appendChild(img);
+                } else {
+                    cellElement.textContent = cell;
+                }
+
+                tr.appendChild(cellElement); // Append the cell to the row
+                previousCell = cellElement; // Set this cell as the last non-empty cell
             }
         });
 
-        table.appendChild(tr);
+        table.appendChild(tr); // Append the row to the table
     });
 
     return table;
@@ -60,13 +69,13 @@ function createTable(data) {
 
 // Function to initialize the data fetching and table creation
 function initializeTable() {
-    const tsvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTi_0g4fsZHLownRxEBfnrAGnzIHRLNOkqVN_ibgrUHDxhXD5WdL3LhlHnrEn0PnGivZjIvjQQ2UL7i/pub?gid=0&single=true&output=tsv'; // Replace with the actual URL
+    const tsvUrl = 'YOUR_TSV_DATA_URL'; // Replace with the actual URL to your TSV data
 
     fetchTSVData(tsvUrl).then(data => {
         const container = document.getElementById('data-container');
-        container.innerHTML = '';
+        container.innerHTML = ''; // Clear any existing content
         const table = createTable(data);
-        container.appendChild(table);
+        container.appendChild(table); // Append the new table to the container
     });
 }
 
