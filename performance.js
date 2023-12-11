4a53a3250f87f3652560c55f70ab489feca99611
Varun-1975 <153108013+Varun-1975@users.noreply.github.com>
2023-12-11 18:18:53 +0530

    Update performance.js

--- performance.js
+++ performance.js
@@ -5,17 +5,35 @@ async function fetchTSVData(url) {
     return text.split('\n').map(row => row.split('\t'));
 }
 
+// Helper function to check if a string is an image link
+function isImageLink(str) {
+    // Here we're checking if the string is a link to an image based on the provided pattern
+    return str.startsWith('https://cdn.discordapp.com/attachments/');
+}
+
 // Function to create a table with the TSV data
 function createTable(data) {
     const table = document.createElement('table');
     table.className = 'data-table'; // Add a class for CSS styling
 
-    data.forEach((row) => {
+    data.forEach((row, rowIndex) => {
         const tr = document.createElement('tr'); // Create a new table row
         let previousCell = null; // To keep track of the last non-empty cell
 
-        row.forEach((cell) => {
-            if (cell.trim() === '') {
+        row.forEach((cell, cellIndex) => {
+            if (rowIndex === 1 && cellIndex === 0) {
+                // This is the cell with the image link
+                const td = document.createElement('td');
+                td.colSpan = row.length; // The image cell spans all columns
+                const img = document.createElement('img');
+                img.src = cell; // Set the image source to the link
+                img.alt = 'Image'; // Set an alt text for the image
+                img.style.maxWidth = '100%'; // Ensure the image is not larger than the cell
+                img.style.height = 'auto';
+                td.appendChild(img);
+                tr.appendChild(td);
+                return; // Skip the rest of the cells in this row
+            } else if (cell.trim() === '') {
                 // If the cell is empty, increase the colspan of the previous cell
                 if (previousCell) previousCell.colSpan += 1;
             } else {
@@ -36,7 +54,7 @@ function createTable(data) {
 // Function to initialize the data fetching and table creation
 function initializeTable() {
     // The URL should point to your TSV data
-    const tsvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTi_0g4fsZHLownRxEBfnrAGnzIHRLNOkqVN_ibgrUHDxhXD5WdL3LhlHnrEn0PnGivZjIvjQQ2UL7i/pub?gid=0&single=true&output=tsv'; // Replace with the actual URL
+    const tsvUrl = 'https://example.com/path/to/your/data.tsv'; // Replace with the actual URL
 
     fetchTSVData(tsvUrl).then(data => {
         const container = document.getElementById('data-container');
