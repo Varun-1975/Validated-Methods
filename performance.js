6cbc10e369aafca085754158aaa277abb0bed02d
Varun-1975 <153108013+Varun-1975@users.noreply.github.com>
2023-12-11 17:56:44 +0530

    Update performance.js

--- performance.js
+++ performance.js
@@ -2,72 +2,58 @@
 async function fetchTSVData(url) {
     const response = await fetch(url);
     const text = await response.text();
-    const rows = text.split('\n').map(row => row.split('\t'));
-    return rows;
-}
-
-// Helper function to create a cell and append it to the row
-function createCell(row, tagName, text, colSpan, isBold) {
-    const cell = document.createElement(tagName);
-    if (isImageLink(text)) {
-        const img = document.createElement('img');
-        img.src = parseImageLink(text);
-        img.style.width = '200px'; // Set the desired width
-        img.style.height = 'auto'; // Height will be set automatically
-        img.style.display = 'block'; // Use block display to apply margin
-        img.style.margin = 'auto'; // Center the image within the cell
-        cell.appendChild(img);
-    } else {
-        cell.textContent = text;
-    }
-    if (colSpan) {
-        cell.setAttribute('colspan', colSpan);
-    }
-    if (isBold) {
-        cell.style.fontWeight = 'bold';
-    }
-    if (tagName === 'th') {
-        cell.style.textAlign = 'center'; // Center the text in header cells
-    }
-    row.appendChild(cell);
+    return text.split('\n').map(row => row.split('\t'));
 }
 
 // Function to create a table with the TSV data
 function createTable(data) {
     const table = document.createElement('table');
+
     data.forEach((row, rowIndex) => {
         const tr = document.createElement('tr');
-        row.forEach((cell, cellIndex) => {
-            let tagName = 'td';
-            let colSpan = null;
-            let isBold = false;
 
-            // Determine if the current cell should be created based on specific merge patterns
+        row.forEach((cell, cellIndex) => {
+            // Create the header row with merged cells
             if (rowIndex === 0 && cellIndex === 0) {
-                // Merged title cell A1:C1
-                colSpan = 3;
-                isBold = true;
-                createCell(tr, 'th', cell, colSpan, isBold);
-            } else if (rowIndex === 1 && cellIndex === 0) {
-                // Merged image cell A2:C2
-                colSpan = 3;
-                createCell(tr, tagName, cell, colSpan, isBold);
-            } else if (rowIndex >= 2 && cellIndex === 0) {
-                // Column A cells should be bold
-                isBold = true;
-                createCell(tr, tagName, cell, colSpan, isBold);
-            } else if (rowIndex >= 2 && cellIndex === 1) {
-                // Cells B3:C3 to B21:C21 are merged horizontally
-                colSpan = 2;
-                createCell(tr, tagName, cell, colSpan, isBold);
-            } else if ((rowIndex === 10 || rowIndex === 16) && cellIndex === 0) {
-                // Merged bold cells A10:C10 and A16:C16
-                colSpan = 3;
-                isBold = true;
-                createCell(tr, 'th', cell, colSpan, isBold);
+                const th = document.createElement('th');
+                th.setAttribute('colspan', '3');
+                th.textContent = cell;
+                th.style.textAlign = 'center';
+                th.style.fontWeight = 'bold';
+                tr.appendChild(th);
+                return; // Skip the rest of the cells in the header row
             }
-            // Cells in column C are skipped because they are merged with column B
+
+            // Handle the image row with merged cells
+            if (rowIndex === 1 && cellIndex === 0) {
+                const th = document.createElement('th');
+                th.setAttribute('colspan', '3');
+                const img = document.createElement('img');
+                img.src = parseImageLink(cell);
+                img.style.display = 'block';
+                img.style.marginLeft = 'auto';
+                img.style.marginRight = 'auto';
+                img.style.width = '200px'; // Set the image size
+                th.appendChild(img);
+                tr.appendChild(th);
+                return; // Skip the rest of the cells in the image row
+            }
+
+            // Skip cells that are meant to be merged
+            if (rowIndex > 1 && cellIndex > 0) return;
+
+            // Handle bold cells and merged cells for the rest of the table
+            const td = document.createElement('td');
+            if (cellIndex === 0) {
+                td.style.fontWeight = 'bold'; // Bold for the first column
+            }
+            if (rowIndex > 1 && cellIndex === 1) {
+                td.setAttribute('colspan', '2'); // Merge horizontally for specified cells
+            }
+            td.textContent = cell;
+            tr.appendChild(td);
         });
+
         table.appendChild(tr);
     });
 
