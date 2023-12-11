d415781072b2b6416a65b38165e00453144618d9
Varun-1975 <153108013+Varun-1975@users.noreply.github.com>
2023-12-11 16:05:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,13 +24,18 @@ async function loadTableFromSheet(url) {
         return;
     }
 
+    // Clear existing data
     container.innerHTML = '';
 
+    // Create table elements
     const table = document.createElement('table');
     const tableHead = document.createElement('thead');
     const tableBody = document.createElement('tbody');
     table.appendChild(tableHead);
     table.appendChild(tableBody);
+
+    // Initialize an array to keep track of the last non-empty cell for each column
+    let lastNonEmptyCell = Array(data[0].length).fill(null);
 
     // Create header row
     let headerRow = document.createElement('tr');
@@ -41,35 +46,43 @@ async function loadTableFromSheet(url) {
     });
     tableHead.appendChild(headerRow);
 
-    // Create table body
-    data.slice(1).forEach((row, rowIndex) => {
-        let tr = document.createElement('tr');
-        row.forEach((cell, cellIndex) => {
-            if (rowIndex === 1 && cellIndex === 0) {
-                // Handle the merged cell for the image
-                let td = document.createElement('td');
-                td.setAttribute('colspan', '3');
-                if (isValidUrl(cell)) {
-                    let img = document.createElement('img');
-                    img.src = cell;
-                    img.style.maxWidth = '100px'; // Set a max width for the image
-                    td.appendChild(img);
-                } else {
-                    td.textContent = cell;
-                }
-                tr.appendChild(td);
-            } else if (!(rowIndex === 1 && (cellIndex === 1 || cellIndex === 2))) {
-                // Skip adding cells for B and C in the second row
-                let td = document.createElement('td');
+    // Create table body and handle merged cells
+    data.slice(1).forEach(row => {
+    let tr = document.createElement('tr');
+    row.forEach((cell, cellIndex) => {
+        let td = document.createElement('td');
+
+        // Check if the current cell is from the URL column (5th column)
+        if (cellIndex === 4 && isValidUrl(cell)) {
+            console.log("URL found:", cell);
+            let previewLink = document.createElement('a');
+            previewLink.href = "#"; // Prevent default link behavior
+            previewLink.textContent = "Preview";
+            previewLink.addEventListener('click', function(event) {
+                event.preventDefault(); // Prevent default link behavior
+                openPreviewWindow(cell); // Open the preview window with the URL
+            });
+            td.appendChild(previewLink);
+        } 
+        if (cell.trim() === '' && lastNonEmptyCell[cellIndex] != null) {
+                // Increment rowspan for the last non-empty cell in this column
+                lastNonEmptyCell[cellIndex].rowSpan += 1;
+            }
+        else {
+            let td = document.createElement('td');
                 td.textContent = cell;
                 tr.appendChild(td);
-            }
-        });
-        tableBody.appendChild(tr);
-    });
+                lastNonEmptyCell[cellIndex] = td; // This becomes the last non-empty cell
+        }
 
+        tr.appendChild(td);
+    });
+    tableBody.appendChild(tr);
+});
+    // Append the table to the container
     container.appendChild(table);
 }
+
 function parseTSV(tsvData) {
     // Split the TSV data into lines and then cells using tab as the delimiter
     return tsvData.split('\n').map(row => row.split('\t'));
